e90e9dc2cc6fe6cdf440693428fbe228fd48ec34
rosewhat <whatcodingrose@gmail.com>
2022-10-19 02:32:38 +0300

    add logic

--- app/src/main/java/com/rosewhat/translate/presentation/MainActivity.kt
+++ app/src/main/java/com/rosewhat/translate/presentation/MainActivity.kt
@@ -2,7 +2,11 @@ package com.rosewhat.translate.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.lifecycle.ViewModelProvider
 import com.rosewhat.translate.databinding.ActivityMainBinding
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +14,38 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        changeText()
+
+
     }
+
+    private fun changeText() {
+        binding.etPassword.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable) {
+                binding.etPassword.removeTextChangedListener(this)
+                val mapping = mapOf("q" to "й")
+                var newStr = ""
+                var newC = ""
+                for ((key, value) in mapping) {
+                    newStr = key
+                    newC = value
+                }
+                val text = s.toString().replace(newStr, newC)
+                s.replace(0, s.length, text)
+
+                binding.etPassword.addTextChangedListener(this)
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        })
+
+
+    }
+
 }
