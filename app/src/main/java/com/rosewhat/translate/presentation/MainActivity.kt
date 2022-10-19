350b2de6d4223bb39a1e862edc787a1c907aaf2b
rosewhat <whatcodingrose@gmail.com>
2022-10-19 18:13:30 +0300

    add method

--- app/src/main/java/com/rosewhat/translate/presentation/MainActivity.kt
+++ app/src/main/java/com/rosewhat/translate/presentation/MainActivity.kt
@@ -25,20 +25,54 @@ class MainActivity : AppCompatActivity() {
 
     private fun changeText() {
         binding.etPassword.addTextChangedListener(object : TextWatcher {
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
 
+            }
             override fun afterTextChanged(s: Editable) {
                 binding.etPassword.removeTextChangedListener(this)
-                val mapping = mapOf("q" to "й")
-                var newStr = ""
-                var newC = ""
-                for ((key, value) in mapping) {
-                    newStr = key
-                    newC = value
-                }
-                val text = s.toString().replace(newStr, newC)
-                s.replace(0, s.length, text)
+                val mapping = mapOf(
+                    "Й" to "Q",
+                    "Ц" to "W",
+                    "У" to "E",
+                    "К" to "R",
+                    "Е" to "T",
+                    "Н" to "Y",
+                    "Г" to "U",
+                    "Ш" to "I",
+                    "Щ" to "O",
+                    "З" to "P",
+                    "Х" to "[",
+                    "Ъ" to "]",
 
+                    "Ф" to "A",
+                    "Ы" to "S",
+                    "В" to "D",
+                    "А" to "F",
+                    "П" to "G",
+                    "Р" to "H",
+                    "О" to "J",
+                    "Л" to "K",
+                    "Д" to "L",
+                    "Ж" to ";",
+                    "Э" to "'",
+
+                    "Я" to "Z",
+                    "Ч" to "X",
+                    "С" to "C",
+                    "М" to "V",
+                    "И" to "B",
+                    "Т" to "N",
+                    "Ь" to "M",
+                    "Б" to ",",
+                    "Ю" to ".",
+
+
+                )
+                var text = s.toString()
+                for ((a, b) in mapping) {
+                    text = text.replace(a, b).replace(a.lowercase(), b.lowercase())
+                }
+                s.replace(0, s.length, text)
                 binding.etPassword.addTextChangedListener(this)
             }
 
